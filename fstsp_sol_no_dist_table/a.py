2a42c8fe8b11c0c9ff0369614b4778f559e91052
PedroGTN <pedrogtneves@gmail.com>
2024-04-10 09:25:55 -0300

    Tabelas antigas arrumadas sem duplicatas

---
+++ fstsp_sol_no_dist_table/a.py
@@ -0,0 +1,13 @@
+old_table = open("table.csv", 'r')
+new_table = open("n_table.csv", 'w')
+
+lines = old_table.readlines()
+
+new_table.write(lines[])
+
+for l in lines[1:]:
+    a = l.split('-')
+    print(a)
+    if a[1][:5] != "alpha":
+        print(a[1][:5])
+        new_table.write(l)
