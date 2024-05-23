f757646fff632811f8f5b303b1c6fbe24c407d47
PedroGTN <pedrogtneves@gmail.com>
2024-05-23 17:11:52 -0300

    Testes de bolhas_elipticas rodados

--- src/single_obj/run_tests/heuristics2csv.py
+++ src/single_obj/run_tests/heuristics2csv.py
@@ -11,6 +11,7 @@ csv_file_name = h_solutions_path + 'table_BE.csv'
 csv_avg_file_name = h_solutions_path + 'table_BE_avg.csv'
 
 
+# node_list = [5, 6, 7, 8, 9, 10, 20, 50, 75, 100, 175, 250, 375, 500]
 node_list = [50, 75, 100]
 alpha_list = [1, 2, 3]
 num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
@@ -21,7 +22,7 @@ solution_type.remove('table_BE.csv')
 solution_type.remove('table_BE_avg.csv')
 
 with open(csv_file_name, 'w') as csv, open (csv_avg_file_name, 'w') as csv_avg:
-    csv_avg.write('instance_name,num_nodes,instance_type,alpha,total_runtime,optimal_tsp_solution,optimal_tsp_split,lkh_cost,lkh_split,best_solution_cost,opt_to_best_dist,opt_to_best_percent,optsplit_to_best_dist,optsplit_to_best_percent\n')
+    csv_avg.write('num_nodes,instance_type,alpha,total_runtime,optimal_tsp_solution,optimal_tsp_split,lkh_cost,lkh_split,best_solution_cost,opt_to_best_dist,opt_to_best_percent,optsplit_to_best_dist,optsplit_to_best_percent\n')
     csv.write('instance_name,num_nodes,instance_type,alpha,total_runtime,optimal_tsp_solution,optimal_tsp_split,lkh_cost,lkh_split,best_solution_name,best_solution_cost,opt_to_best_dist,opt_to_best_percent,optsplit_to_best_dist,optsplit_to_best_percent\n')
     for n in node_list:
         for a in alpha_list:
@@ -67,16 +68,16 @@ with open(csv_file_name, 'w') as csv, open (csv_avg_file_name, 'w') as csv_avg:
                         with open(sol_path) as sol, open(tsp_solution, 'w') as tspsol:
 
                             sol_lines = sol.readlines()
-                            tspsol.write(sol_lines[8].split(':')[2][1:])
+                            tspsol.write(sol_lines[9].split(':')[2][1:])
                             csv.write(s[:-4] + ',' + str(n) + ',' + t + ',' + str(alpha) + ',')
-                            total_runtime = float(sol_lines[0].split(':')[1].strip()) + float(sol_lines[1].split(':')[1].strip()) + float(sol_lines[2].split(':')[1].strip())
-                            opt_tsp = float(sol_lines[4].split(':')[2].strip())
-                            opt_tsp_split = float(sol_lines[10].split(':')[2].strip())
+                            total_runtime = float(sol_lines[0].split(':')[1].strip()) + float(sol_lines[1].split(':')[1].strip()) + float(sol_lines[2].split(':')[1].strip()) + float(sol_lines[3].split(':')[1].strip())
+                            opt_tsp = float(sol_lines[5].split(':')[2].strip())
+                            opt_tsp_split = float(sol_lines[11].split(':')[2].strip())
                             csv.write(("{:.4f}".format(total_runtime)) + ',' + str(opt_tsp) + ',' + str(opt_tsp_split) + ',')
-                            csv.write(("{:.4f},".format(float(sol_lines[5].split(':')[2].strip()))))
-                            csv.write(("{:.4f},".format(float(sol_lines[13].split(':')[2].strip()))))
-                            best_sol = float(sol_lines[7].split(':')[1].strip())
-                            csv.write(sol_lines[6].split(':')[1].strip() + ',' + str(best_sol) + ',')
+                            csv.write(("{:.4f},".format(float(sol_lines[6].split(':')[2].strip()))))
+                            csv.write(("{:.4f},".format(float(sol_lines[14].split(':')[2].strip()))))
+                            best_sol = float(sol_lines[8].split(':')[1].strip())
+                            csv.write(sol_lines[7].split(':')[1].strip() + ',' + str(best_sol) + ',')
                             csv.write(("{:.4f}".format(best_sol - opt_tsp)) + ',')
                             csv.write(("{:.4f}%".format(100 * best_sol/opt_tsp)) + ',')
                             csv.write(("{:.4f}".format(best_sol - opt_tsp_split)) + ',')
@@ -85,8 +86,8 @@ with open(csv_file_name, 'w') as csv, open (csv_avg_file_name, 'w') as csv_avg:
                             opt_tsp_avg += opt_tsp
                             opt_tsp_split_avg += opt_tsp_split
                             best_sol_avg += best_sol
-                            lkh_split_avg += float(sol_lines[13].split(':')[2].strip())
-                            lkh_avg += float(sol_lines[5].split(':')[2].strip())
+                            lkh_split_avg += float(sol_lines[14].split(':')[2].strip())
+                            lkh_avg += float(sol_lines[6].split(':')[2].strip())
                     
                 csv_avg.write(str(n) + ',' + t + ',' + str(a) + ',')
                 csv_avg.write(("{:.4f},".format(total_runtime_avg/count)) + ("{:.4f},".format(opt_tsp_avg/count)) + ("{:.4f},".format(opt_tsp_split_avg/count)))
