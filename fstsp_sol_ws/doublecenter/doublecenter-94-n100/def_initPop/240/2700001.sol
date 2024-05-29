4300ecdd52f7017f0d9d0559406f752accb3f476
--replace-all <--get>
2024-05-29 10:30:04 -0300

    mais testes

--- fstsp_sol_ws/doublecenter/doublecenter-94-n100/def_initPop/240/2700001.sol
+++ fstsp_sol_ws/doublecenter/doublecenter-94-n100/def_initPop/240/2700001.sol
@@ -1,2 +1,26 @@
 Custom stopping criteria not supplied by the user. Using max. time = 240s and max. stall_offset = 0
 Using 1 threads for decoding
+initialization time: 0.110209
+best warm start solution: 1293.37
+TSP tour:
+0 81 6 20 82 17 71 35 19 87 88 40 28 13 98 22 61 72 92 93 16 96 44 29 2 68 47 63 9 66 48 51 67 75 49 43 86 21 85 80 70 23 38 32 59 95 5 27 60 30 55 83 91 34 77 54 33 12 1 73 79 65 26 4 76 31 42 74 90 58 94 24 99 10 62 50 45 52 89 56 78 14 97 15 11 53 57 46 64 25 69 8 39 3 18 7 37 84 36 41 
+Operations:
+0,6,82|82,17,35|35,98,22|22,72,92|92,93,16|16,44,29|29,2,68|68,63,9|9,66,48|48,43,80|80,70,38|38,95,5|5,27,60|60,30,83|83,34,79|79,65,26|26,4,42|42,74,90|90,58,94|94,24,10|10,50,45|45,52,89|89,56,14|14,97,53|53,57,46|46,25,69|69,8,39|39,3,18|18,37,36|36,41,0|
+method: def_initPop
+seed: 2700001
+Instance: 1
+best_fitness: 1099.02
+current_iteration: 7485
+last_update_iteration: 992
+current_time: 240.028s
+last_update_time: 30.3287s
+largest_iteration_offset: 489
+stalled_iterations: 6493
+path_relink_time: 0s
+num_path_relink_calls: 0
+num_homogenities: 0
+num_best_improvements: 0
+num_elite_improvements: 0
+num_exchanges: 0
+num_shakes: 0
+num_resets: 0
---
+++ fstsp_sol_ws/doublecenter/doublecenter-94-n100/def_initPop/240/8434820.sol
@@ -0,0 +1,2 @@
+Custom stopping criteria not supplied by the user. Using max. time = 240s and max. stall_offset = 0
+Using 1 threads for decoding
